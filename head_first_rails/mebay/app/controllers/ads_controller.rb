01c7f517fd9b9fc2c7b1e8fa8673ea7fbe443ff0
nkoch <none@none>
2009-07-06 01:17:30 +0000

    added redirection from /ads/create/ to /ads/show
    --HG--
    branch : rails
    extra : convert_revision : svn%3Aebaea464-3003-428c-941a-1d5eb6580de9/tutorials/rails%40387

--- head_first_rails/mebay/app/controllers/ads_controller.rb
+++ head_first_rails/mebay/app/controllers/ads_controller.rb
@@ -8,4 +8,13 @@ class AdsController < ApplicationController
     @ad = Ad.find(params[:id])
   end
   
+  def new
+    @ad = Ad.new
+  end
+  
+  def create
+    @ad = Ad.new(params[:ad])
+    @ad.save
+    redirect_to "/ads/#{ @ad.id }"
+  end
 end
