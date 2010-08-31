0ce4a51538196176f131e05d23b95418c36baadf
Nathan Koch <nathanrkoch@gmail.com>
2010-08-30 21:11:39 -0500

    depot scaffold

---
+++ agile-web-development-with-rails/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ded93cf2343f6b1e0e78da7cb93a8d8d50cac899253fe68e7d437fb208f205c618de6fbec805438f23a60b482eb04ec7a0141e9677c1a2f5f884871b2b5d6dcc'
